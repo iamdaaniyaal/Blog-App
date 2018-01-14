e2f67cae34b58cad63c5c39202d95278d089bd8a
Mohammed Daaniyaal <35423314+iamdaaniyaal@users.noreply.github.com>
2018-01-14 19:56:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ app.secret_key = os.urandom(24)
 
 # MySQL configurations
 
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
-app.config['MYSQL_DATABASE_DB'] = 'blogapp'
+app.config['MYSQL_DATABASE_USER'] = 'user'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'password'
+app.config['MYSQL_DATABASE_DB'] = 'database'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
